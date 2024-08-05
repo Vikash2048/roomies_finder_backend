c1cbc0966a0e882931ea9c865f5d221b6473e015
Vikash2048 <rpsvikash16@gmail.com>
2024-08-05 22:28:16 +0530

    created apiresponse handler errorhandler and function handler

---
+++ src/utils/errorHandling.js
@@ -0,0 +1,15 @@
+class errorHandling extends Error {
+    constructor(statusCode,
+        message="Something went wrong",
+        errors = []
+    ){
+        super(message)
+        this.message = message
+        this.statusCode = statusCode
+        this.data = null
+        this.errors = errors
+        this.success = false;
+    }
+}
+
+export {errorHandling};
